3a674ede09346804ea3a3474f29de4ef7a702bbd
Ankit Kumar <ankitydv098@gmail.com>
2026-07-27 05:33:14 +0530

    Time: 1 ms (77.35%), Space: 42.4 MB (67.39%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,28 +1,56 @@
-import java.util.HashSet;
+// import java.util.HashSet;
 
 class Solution {
-    public boolean isHappy(int n) {
-
-        HashSet<Integer> set = new HashSet<>();
-
-        while (n != 1 && !set.contains(n)) {
-            set.add(n);
-            n = squareSum(n);
+    // --> sum of square of digits 
+    public int sumofsquareofdigit(int n){
+        int sum =0;
+        while(n>0){
+            int dig = n%10;
+            sum = sum + (dig*dig);
+            n=n/10;
         }
-
-        return n == 1;
-    }
-
-    public int squareSum(int n) {
-
-        int sum = 0;
-
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
-        }
-
         return sum;
+
+
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(fast!=1){
+            slow =sumofsquareofdigit(slow);
+            fast=sumofsquareofdigit(sumofsquareofdigit(fast));
+            if(fast==1){
+                return true;
+            }
+            if(slow==fast){
+                return false;
+            }
+         
+        }
+           return true;
     }
 }
+
+    //     HashSet<Integer> set = new HashSet<>();
+
+    //     while (n != 1 && !set.contains(n)) {
+    //         set.add(n);
+    //         n = squareSum(n);
+    //     }
+
+    //     return n == 1;
+    // }
+
+    // public int squareSum(int n) {
+
+    //     int sum = 0;
+
+    //     while (n > 0) {
+    //         int digit = n % 10;
+    //         sum += digit * digit;
+    //         n /= 10;
+    //     }
+
+    //     return sum;
+    
