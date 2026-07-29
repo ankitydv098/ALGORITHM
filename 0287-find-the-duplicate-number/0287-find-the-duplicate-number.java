f9406a68aeba654fcbb96bdc0abaf540dfed6439
Ankit Kumar <ankitydv098@gmail.com>
2026-07-29 15:42:07 +0530

    Time: 4 ms (91.06%), Space: 83 MB (47.45%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,35 +1,65 @@
 class Solution {
     
-     public int  findDuplicate(int[] arr){
+     public int  findDuplicate(int[] nums){
 
-        int i = 0;
+        int slow = 0;
+        int fast=0;
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
 
-        while (i< arr.length){
-
-            if(arr[i] != i + 1){
-                int correct = arr[i] - 1;
-                if (arr[i] != arr[correct] ) {
-                    swap( arr, i , correct);
-                }
-                else {
-                   return arr[i];
-                }
-
-            }
-            else{
-                i++;
+            if(slow==fast){
+                break;
             }
 
+        } while(slow != fast);
 
+        int n1=0,
+            n2=slow;
+
+        while(n1 != n2){
+            n1=nums[n1];
+            n2=nums[n2];
         }
-        return -1;
-    }
+    return n1;
 
 
-    static void swap(int[] arr, int first, int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+
+
+
+
+
+    //     int i = 0;
+
+    //     while (i< arr.length){
+
+    //         if(arr[i] != i + 1){
+    //             int correct = arr[i] - 1;
+    //             if (arr[i] != arr[correct] ) {
+    //                 swap( arr, i , correct);
+    //             }
+    //             else {
+    //                return arr[i];
+    //             }
+
+    //         }
+    //         else{
+    //             i++;
+    //         }
+
+
+    //     }
+    //     return -1;
+    // }
+
+
+    // static void swap(int[] arr, int first, int second){
+    //     int temp = arr[first];
+    //     arr[first] = arr[second];
+    //     arr[second] = temp;
+
+
+
     }
 
 }
