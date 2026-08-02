d565eee1f01ac84cb2b55a5a5b2a9462ed65a38b
Ankit Kumar <ankitydv098@gmail.com>
2026-08-02 15:59:01 +0530

    Time: 56 ms (48.74%), Space: 99.5 MB (23.9%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,88 +1,30 @@
-// class Solution {
-//     public long maximumSubarraySum(int[] nums, int k) {
-
-//         HashMap<Integer, Integer> map = new HashMap<>();
-
-//         long sum = 0;
-//         long ans = 0;
-//         int left = 0;
-
-//         for (int right = 0; right < nums.length; right++) {
-
-//             sum += nums[right];
-//             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-
-//             if (right - left + 1 > k) {
-//                 sum -= nums[left];
-
-//                 int freq = map.get(nums[left]) - 1;
-//                 if (freq == 0)
-//                     map.remove(nums[left]);
-//                 else
-//                     map.put(nums[left], freq);
-
-//                 left++;
-//             }
-
-//             if (right - left + 1 == k && map.size() == k) {
-//                 ans = Math.max(ans, sum);
-//             }
-//         }
-
-//         return ans;
-//     }
-// }
-
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-
-        int n = nums.length;
-
-        int maxValue = 0;
-        for (int x : nums)
-            maxValue = Math.max(maxValue, x);
-
-        int[] freq = new int[maxValue + 1];
-
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int i=0,j=0;
+        long res = 0;
         long sum = 0;
-        long maxSum = 0;
-        int distinct = 0;
 
-        // First window
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
+        while(j<nums.length) {
+           map.put(nums[j],map.getOrDefault(nums[j],0) + 1);
+           sum += nums[j];
 
-            if (freq[nums[i]] == 0)
-                distinct++;
+           if(j-i+1 > k) {
+                sum -= nums[i];
+                map.put(nums[i],map.get(nums[i]) - 1);
 
-            freq[nums[i]]++;
+                if(map.get(nums[i])  == 0) {
+                    map.remove(nums[i]);
+                }
+                i++;
+           }
+
+           if(j-i+1 == k && map.size() == k) {
+            res = Math.max(res,sum);
+           }
+           
+           j++;
         }
-
-        if (distinct == k)
-            maxSum = sum;
-
-        // Sliding window
-        for (int i = k; i < n; i++) {
-
-            int left = nums[i - k];
-            freq[left]--;
-            sum -= left;
-
-            if (freq[left] == 0)
-                distinct--;
-
-            int right = nums[i];
-
-            if (freq[right] == 0)
-                distinct++;
-
-            freq[right]++;
-            sum += right;
-
-            if (distinct == k)
-                maxSum = Math.max(maxSum, sum);
-        }
-
-        return maxSum;
+        return res;
     }
 }
