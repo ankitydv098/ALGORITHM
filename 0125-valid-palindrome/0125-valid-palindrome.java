3e299f58933c8955ddb74a3547a04c30708c644a
Ankit Kumar <ankitydv098@gmail.com>
2026-08-05 00:17:11 +0530

    Time: 14 ms (29.38%), Space: 45.8 MB (35.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null) {
+        if(s==null){
             return false;
-            
         }
-     
-        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();   
+        String n = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); 
         int i = 0;
-        int j = cleaned.length() - 1;
-
-        while (i < j) {
-            if (cleaned.charAt(i) != cleaned.charAt(j)) {
+        int j=n.length()-1;
+        while(i<j){
+            if(n.charAt(i)!=n.charAt(j)){
                 return false;
             }
             i++;
             j--;
         }
-
         return true;
-        
-        
     }
 }
